0b0a84cdd26ce44172c35c5061b17648ad60555e
Aiyern30 <ianbian2@gmail.com>
2025-02-12 21:53:10 +0800

    Added gameplayer.ts

---
+++ type/NBA/gamePlayer.ts
@@ -0,0 +1,46 @@
+// TypeScript types based on the sample response
+
+interface FieldGoals {
+    total: number;
+    attempts: number;
+    percentage: number | null;
+  }
+  
+  interface ThreePointGoals {
+    total: number;
+    attempts: number;
+    percentage: number | null;
+  }
+  
+  interface FreeThrowsGoals {
+    total: number;
+    attempts: number;
+    percentage: number | null;
+  }
+  
+  interface Rebounds {
+    total: number;
+  }
+  
+  export interface PlayerStats {
+    game: {
+      id: number;
+    };
+    team: {
+      id: number;
+    };
+    player: {
+      id: number;
+      name: string;
+    };
+    type: string;
+    minutes: string;
+    field_goals: FieldGoals;
+    threepoint_goals: ThreePointGoals;
+    freethrows_goals: FreeThrowsGoals;
+    rebounds: Rebounds;
+    assists: number;
+    points: number;
+  }
+  
+ 
