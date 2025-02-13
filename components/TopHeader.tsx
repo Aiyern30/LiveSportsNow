4bde2930a8bfd18ee237a383487ee76a7fb77bf7
Aiyern30 <ianbian2@gmail.com>
2025-02-13 19:27:20 +0800

    Added function to fetch seasons

--- components/TopHeader.tsx
+++ components/TopHeader.tsx
@@ -1,34 +1,45 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import {
   SidebarTrigger,
-  Breadcrumb,
-  BreadcrumbList,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbSeparator,
-  BreadcrumbPage,
   Separator,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
 } from "./ui";
+import { fetchSeasons } from "@/utils/Seasons/fetchSeasons";
 
 const TopHeader = () => {
+  const [seasons, setSeasons] = useState<string[]>([]);
+
+  useEffect(() => {
+    const getSeasons = async () => {
+      const fetchedSeasons = await fetchSeasons();
+      setSeasons(fetchedSeasons);
+    };
+
+    getSeasons();
+  }, []);
   return (
     <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 ">
       <div className="flex items-center gap-2 px-4">
         <SidebarTrigger className="-ml-1" />
         <Separator orientation="vertical" className="mr-2 h-4" />
-        <Breadcrumb>
-          <BreadcrumbList>
-            <BreadcrumbItem className="hidden md:block">
-              <BreadcrumbLink href="#">
-                Building Your Application
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator className="hidden md:block" />
-            <BreadcrumbItem>
-              <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-            </BreadcrumbItem>
-          </BreadcrumbList>
-        </Breadcrumb>
+        <Select>
+          <SelectTrigger className="w-[280px]">
+            <SelectValue placeholder="Select a season" />
+          </SelectTrigger>
+          <SelectContent>
+            {seasons.map((season) => (
+              <SelectItem key={season} value={season.toString()}>
+                {season}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
       </div>
     </header>
   );
