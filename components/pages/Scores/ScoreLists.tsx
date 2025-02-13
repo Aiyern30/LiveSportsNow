ef5b3cb0d31483b648f5f9f084eabbfb5bd54e7e
Aiyern30 <ianbian2@gmail.com>
2025-02-13 09:51:12 +0800

    Create ScoresDialog components for showing NBA Player

--- components/pages/Scores/ScoreLists.tsx
+++ components/pages/Scores/ScoreLists.tsx
@@ -2,23 +2,10 @@ import React, { FC, useEffect, useState } from "react";
 import Image from "next/image";
 import { NBAGame } from "@/type/NBA/game";
 import { useDeviceType } from "@/lib/useDevicesType";
-import {
-  Button,
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui";
+import { Button } from "@/components/ui";
 import { fetchNBAPlayerStatsByGameId } from "@/utils/NBA/fetchNBAPlayerStatsByGameId";
 import { PlayerStats } from "@/type/NBA/gamePlayer";
+import ScoresDialog from "./ScoresDialog";
 
 interface ListsProps {
   filteredGames: NBAGame[];
@@ -165,90 +152,11 @@ const ScoreLists: FC<ListsProps> = ({ filteredGames }) => {
       ))}
 
       {dialogOpen && (
-        <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
-          <DialogContent className="sm:max-w-[425px] md:max-w-[600px] lg:max-w-[800px] max-h-[80vh] overflow-auto">
-            <DialogHeader>
-              <DialogTitle>Top Players for Game {selectedGameId}</DialogTitle>
-              <DialogDescription>
-                Here are the top players for this game. Review their stats.
-              </DialogDescription>
-            </DialogHeader>
-            <div className="overflow-x-auto">
-              <Table className="min-w-[500px]">
-                <TableHeader>
-                  <TableRow>
-                    <TableHead className="w-[200px]">Player</TableHead>
-                    <TableHead className="text-right">Minutes</TableHead>
-                    <TableHead className="text-right">Points</TableHead>
-                    <TableHead className="text-right">Assists</TableHead>
-                    <TableHead className="text-right">Rebounds</TableHead>
-                    <TableHead className="text-right">Field Goals</TableHead>
-                    <TableHead className="text-right">3P Goals</TableHead>
-                    <TableHead className="text-right">FT Goals</TableHead>
-                    <TableHead className="text-right">Type</TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  {nbaPlayer.map((player) => (
-                    <TableRow key={player.player.id}>
-                      {/* Player Name */}
-                      <TableCell className="font-medium">
-                        {player.player.name}
-                      </TableCell>
-                      {/* Points */}
-                      <TableCell className="text-right">
-                        {player.minutes}
-                      </TableCell>
-
-                      {/* Points */}
-                      <TableCell className="text-right">
-                        {player.points}
-                      </TableCell>
-
-                      {/* Assists */}
-                      <TableCell className="text-right">
-                        {player.assists}
-                      </TableCell>
-
-                      {/* Rebounds */}
-                      <TableCell className="text-right">
-                        {player.rebounds.total}
-                      </TableCell>
-
-                      {/* Field Goals (total/attempts) */}
-                      <TableCell className="text-right">
-                        {player.field_goals.total} /{" "}
-                        {player.field_goals.attempts}
-                      </TableCell>
-
-                      {/* Three-point Goals (total/attempts) */}
-                      <TableCell className="text-right">
-                        {player.threepoint_goals.total} /{" "}
-                        {player.threepoint_goals.attempts}
-                      </TableCell>
-
-                      {/* Free Throws (total/attempts) */}
-                      <TableCell className="text-right">
-                        {player.freethrows_goals.total} /{" "}
-                        {player.freethrows_goals.attempts}
-                      </TableCell>
-
-                      {/* Player Type */}
-                      <TableCell className="text-right">
-                        {player.type}
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </div>
-            <DialogFooter>
-              <Button type="button" onClick={() => setDialogOpen(false)}>
-                Close
-              </Button>
-            </DialogFooter>
-          </DialogContent>
-        </Dialog>
+        <ScoresDialog
+          dialogOpen={dialogOpen}
+          setDialogOpen={setDialogOpen}
+          nbaPlayer={nbaPlayer}
+        />
       )}
     </div>
   );
